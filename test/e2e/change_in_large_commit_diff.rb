37b58fe0a0ab23fc31a8766cd1a5c061f2145ce4
Igor Šarčević <igor@renderedtext.com>
2021-03-30 17:10:42 +0200

    Simulate change_in with shallow branches that need deepening

---
+++ test/e2e/change_in_large_commit_diff.rb
@@ -0,0 +1,67 @@
+# rubocop:disable all
+
+require_relative "../e2e"
+require 'yaml'
+
+pipeline = %{
+version: v1.0
+name: Test
+agent:
+  machine:
+    type: e1-standard-2
+
+blocks:
+  - name: Test
+    run:
+      when: "branch = 'master' and change_in('/lib')"
+    task:
+      jobs:
+        - name: Hello
+          commands:
+            - echo "Hello World"
+}
+
+origin = TestRepoForChangeIn.setup()
+
+origin.add_file('.semaphore/semaphore.yml', pipeline)
+origin.commit!("Bootstrap")
+
+origin.add_file("lib/A.txt", "hello")
+origin.commit!("Changes on master")
+
+origin.create_branch("dev")
+
+100.times do |index|
+  origin.add_file("lib/B#{index}.txt", "hello")
+  origin.commit!("Changes in dev number #{index}")
+end
+
+repo = origin.clone_local_copy(branch: "dev")
+repo.run("#{spc} evaluate change-in --input .semaphore/semaphore.yml --output /tmp/output.yml --logs /tmp/logs.yml")
+
+assert_eq(YAML.load_file('/tmp/output.yml'), YAML.load(%{
+version: v1.0
+name: Test
+agent:
+  machine:
+    type: e1-standard-2
+
+blocks:
+  - name: Test
+    run:
+      when: "(branch = 'master') and true"
+    task:
+      jobs:
+        - name: Hello
+          commands:
+            - echo "Hello World"
+
+  - name: Test2
+    run:
+      when: "(branch = 'master') and false"
+    task:
+      jobs:
+        - name: Hello
+          commands:
+            - echo "Hello World"
+}))
